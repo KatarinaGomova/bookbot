3128a2c7d7a56212bd04b2b6bd80a92cbcee26ea
KatarinaGomova <katarina.gomova23@gmail.com>
2025-02-26 15:28:34 +0100

    updated stats and main

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import sys
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-        #print(f"{file_contents}")
-        #get_char_numbers(file_contents)
+    
 
 from stats import count_words
 from stats import get_char_numbers
+from stats import sort_char_numbers
 
 
 
 def main():
-    path = "/home/ajunara/workspace/github.com/bookbot/books/frankenstein.txt"
-    #get_book_text(path)
-    count_words(get_book_text(path))
-    get_char_numbers(get_book_text(path))
+    path = sys.argv
+    if len(path) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(get_book_text(path[1]))} total words")
+    print("----------- Word Count ----------")
+    sort_char_numbers(get_char_numbers(get_book_text(path[1])))
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -5,7 +5,7 @@ def count_words(book_text):
     text = book_text.split()
     for word in text:
         num_words += 1
-    print(f"{num_words} words found in the document")
+    return num_words
 
 
 def get_char_numbers(book_text):
@@ -16,4 +16,21 @@ def get_char_numbers(book_text):
             char_dict[char] += 1
         else:
             char_dict[char] = 1
-    print(f"{char_dict}")
+    return char_dict
+
+
+def sort_char_numbers(dict_of_chars):
+    char_list = []
+    for char, count in dict_of_chars.items():
+        if char.isalpha():
+            char_list.append({char: count})
+
+    def get_count(dict):
+        return list(dict.values())[0]
+
+    char_list.sort(reverse=True, key=get_count)
+    
+    for letter in char_list:
+        for character, count in letter.items():
+            print(f"{character}: {count}")
+   
